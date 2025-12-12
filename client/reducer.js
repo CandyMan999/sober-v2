9e04ad3d622fd54d4d5054a352a82a2c7d38de82
JOHN A LEWIS <jalewis1221@gmail.com>
2025-12-12 09:51:24 -0600

    Simplify quote append reducer

--- client/reducer.js
+++ client/reducer.js
@@ -1,33 +1,15 @@
-const dedupeById = (items = []) => {
-  const seen = new Set();
-  return items.filter((item) => {
-    const id = item?.id;
-    if (!id || seen.has(id)) return false;
-    seen.add(id);
-    return true;
-  });
-};
-
 export default function reducer(state, { type, payload }) {
   switch (type) {
     case "APPEND_PROFILE_QUOTE": {
       if (!payload) return state;
-
       const overview = state?.profileOverview || {};
       const existingQuotes = overview.quotes || [];
-      const overviewUser = overview.user || state?.user || null;
-
-      const nextQuotes = dedupeById([
-        payload,
-        ...existingQuotes.filter((quote) => quote?.id !== payload?.id),
-      ]);
 
       return {
         ...state,
         profileOverview: {
           ...overview,
-          user: overviewUser,
-          quotes: nextQuotes,
+          quotes: [...existingQuotes, payload],
         },
       };
     }
